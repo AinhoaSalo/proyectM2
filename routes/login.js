4b97986efba33451b22632bede1c72fb0f5ec6d0
AinhoaSalo <94933683+AinhoaSalo@users.noreply.github.com>
2022-03-07 13:27:32 +0100

    description home

--- routes/login.js
+++ routes/login.js
@@ -11,16 +11,16 @@ router.post("/", function (req, res) {
   .find({ nameUserRegister: userName })
   .toArray(function (err, arrayuser) {
     if (err !== undefined) {
-      res.send({ mensaje: "Algo no ha salido bien" });
+      res.send({ mensaje: "Error, algo no va bien." });
     } else {
       if (arrayuser.length > 0) {
         if (bcrypt.compareSync(password, arrayuser[0].passwordUserRegister)) {
           res.send({ login: true });
         } else {
-          res.send({ login: false, message: "contraseña incorrecta" });
+          res.send({ login: false, message: "Contraseña incorrecta." });
         }
       } else {
-        res.send({ login: false, message: "registrate por favor que no estás" });
+        res.send({ login: false, message: "Usuario no registrado." });
       }
     }
   });
